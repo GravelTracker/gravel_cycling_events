730eaba9e9cd6098b5a2d742c070fb135afe602f
Tyler Porter <tyler.b.porter@gmail.com>
2020-01-31 00:47:17 -0500

    Suppress Mongoid debug logs in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,4 +105,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  # Suppress log in console in production
+  config.mongoid.logger.level = Logger::INFO
 end
